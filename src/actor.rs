3aa24bbd0401342d59cf7a38e89af211052085e8
chenmx00 <chenmx00@yahoo.com>
2025-11-28 16:34:13 +0000

    main logic of the actor

--- src/actor.rs
+++ src/actor.rs
@@ -5,8 +5,9 @@ use alloy::{
     rpc::client::{ClientBuilder, RpcClient},
 };
 use alloy_transport_ws::WsConnect;
-use std::collections::HashMap;
+use anyhow::{self, Ok};
 use std::sync::Arc;
+use std::{collections::HashMap, ops::Add};
 use tokio::sync::mpsc;
 struct PoolState {}
 enum PoolMessage {
@@ -15,15 +16,15 @@ enum PoolMessage {
         send_to: mpsc::Sender<Option<PoolState>>,
     },
     SimulateSwap {
-        token_in: u128,
+        token_in: U256,
         pool: Address,
-        send_to: mpsc::Sender<Option<u128>>,
+        send_to: mpsc::Sender<Option<U256>>,
     },
     FindPool {
         token_0: Address,
         token_1: Address,
-        fee: u128,
-        send_to: mpsc::Sender<Option<PoolState>>,
+        fee: U256,
+        send_to: mpsc::Sender<Option<Address>>,
     },
 }
 struct PoolActor {
@@ -47,17 +48,65 @@ impl PoolActor {
         }
     }
 
-    async fn on_message(&self, message: PoolMessage) {
+    async fn on_message(&self, message: PoolMessage) -> anyhow::Result<()> {
         match message {
-            PoolMessage::GetPoolState { pool, send_to } => { self.get_pool_state()}
+            PoolMessage::GetPoolState { pool, send_to } => {
+                let pool_state = self.get_pool_state(&pool);
+                let _ = send_to.send(pool_state);
+                Ok(())
+            }
+            PoolMessage::FindPool {
+                token_0,
+                token_1,
+                fee,
+                send_to,
+            } => {
+                let pool_address = self.find_pool(&token_0, &token_1, fee);
+                let _ = send_to.send(pool_address);
+                Ok(())
+            }
+            PoolMessage::SimulateSwap {
+                token_in,
+                pool,
+                send_to,
+            } => {
+                let token_out = self.simulate_swap(token_in, &pool);
+                let _ = send_to.send(token_out);
+                Ok(())
+            }
         }
     }
 
-    async fn simulate_swap() {}
+    fn simulate_swap(&self, token_in: U256, pool: &Address) -> Option<U256> {
+        println!("{:?}", pool);
+        todo!();
+    }
 
-    async fn get_pool_state() {}
+    fn get_pool_state(&self, pool: &Address) -> Option<PoolState> {
+        println!("{:?}", pool);
+        todo!();
+    }
 
-    async fn run() {}
+    fn find_pool(&self, token_0: &Address, token_1: &Address, fee: U256) -> Option<Address> {
+        todo!();
+    }
+
+    async fn index_pools(&mut self) -> anyhow::Result<()> {
+        todo!();
+    }
+
+    async fn sync_pools(&mut self) -> anyhow::Result<()> {
+        todo!();
+    }
+
+    async fn run(&mut self) -> anyhow::Result<()> {
+        println!("Start indexing pools");
+        if let Err(e) = self.index_pools().await {
+            eprintln!("Error encountered indexing pools: {:?}", e);
+            return Err(e);
+        }
+        Ok(())
+    }
 }
 
 struct PoolActorHandler {}
